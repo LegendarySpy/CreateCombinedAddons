f5d3b1a70ffb1d547244beecb2a21a5a010ce3a6
Garon <garon@garon-mbp.myfiosgateway.com>
2023-07-31 02:12:02 -0400

    Initial commit

---
+++ src/main/java/com/legendaryspy/createcombinedaddons/CreateCombinedAddonsClient.java
@@ -0,0 +1,10 @@
+package com.legendaryspy.createcombinedaddons;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CreateCombinedAddonsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/legendaryspy/createcombinedaddons/CreateCombinedAddonsModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.legendaryspy.createcombinedaddons;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CreateCombinedAddonsModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
